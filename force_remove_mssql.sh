ad0eff02824eb51c47ed2a63f6317c3324ec047a
Jack Moon <rokmc756@gmail.com>
2023-05-09 00:06:51 +0900

    Added force remove script

--- force_remove_mssql.sh
+++ force_remove_mssql.sh
@@ -8,11 +8,11 @@ ALL_HOSTS="rk8-single-db"
 for i in $(echo $ALL_HOSTS)
 do
 
-   ssh root@$i "firewall-cmd --zone=public --permanent --remove-port={1521,5500,5520,3938}/tcp"
+   ssh root@$i "firewall-cmd --zone=public --permanent --remove-port=1433/tcp"
    ssh root@$i "firewall-cmd --reload"
-   ssh root@$i "for i in \$(echo 'oinstall dba oper backupdba dgdba kmdba racdba'); do groupdel \$i; done"
-   ssh root@$i "userdel oracle"
-   ssh root@$i "rm -f /etc/security/limits.d/30-oracle-limits.conf /etc/sysctl.d/98-oracle-sysctl.conf"
-   ssh root@$i "rm -rf /u01 /u02 /etc/oraInst.loc /etc/oratab /home/oracle"
+   ssh root@$i "systemctl stop mssql-server"
+   ssh root@$i "yum remove mssql-server -y"
+   ssh root@$i "rm -rf /var/opt/mssql"
+   ssh root@$i "systemctl status mssql-server"
 
 done
